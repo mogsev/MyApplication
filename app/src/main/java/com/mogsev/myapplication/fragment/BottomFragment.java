8698a702e18b16e70c7223d1c22bd97e9f483363
zhenya <mogsev@gmail.com>
2015-07-02 16:16:37 +0300

    update work

---
+++ app/src/main/java/com/mogsev/myapplication/fragment/BottomFragment.java
@@ -0,0 +1,19 @@
+package com.mogsev.myapplication.fragment;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomFragment extends Fragment {
+
+    public BottomFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
