5e015f36a0fee2bec3e938975315d0bd528ae4f7
zhenya <mogsev@gmail.com>
2015-07-09 00:20:25 +0300

    update home

--- app/src/main/java/com/mogsev/myapplication/activity/DivisionActivity.java
+++ app/src/main/java/com/mogsev/myapplication/activity/DivisionActivity.java
@@ -1,9 +1,6 @@
 package com.mogsev.myapplication.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.mogsev.myapplication.R;
 import com.mogsev.myapplication.util.MathOperation;
