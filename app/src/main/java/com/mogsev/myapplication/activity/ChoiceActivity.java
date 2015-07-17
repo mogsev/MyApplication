aa2e6be7ae32bc35a5d921b7cf3e82b85d8a2b01
zhenya <mogsev@gmail.com>
2015-07-17 15:37:56 +0300

    update work

---
+++ app/src/main/java/com/mogsev/myapplication/activity/ChoiceActivity.java
@@ -0,0 +1,17 @@
+package com.mogsev.myapplication.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mogsev.myapplication.R;
+
+public class ChoiceActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+    }
+}
---
+++ app/src/main/java/com/mogsev/myapplication/activity/MultiActivity.java
@@ -0,0 +1,45 @@
+package com.mogsev.myapplication.activity;
+
+import android.os.Bundle;
+
+import com.mogsev.myapplication.R;
+import com.mogsev.myapplication.util.MathOperation;
+import com.mogsev.myapplication.util.MathResult;
+import com.mogsev.myapplication.util.MathTraining;
+import com.mogsev.myapplication.util.RandomValue;
+
+public class MultiActivity extends MathTraining {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi);
+
+
+        //Initialize links for objects
+        initElements();
+
+        //The application was lunched?
+        if (savedInstanceState == null) {
+            mathResult = new MathResult(MathOperation.DIVISION);
+            randomValue = new RandomValue(MathOperation.DIVISION);
+            randomValue.generateExpression(mathResult.getLevel());
+        } else {
+            randomValue = (RandomValue) savedInstanceState.get(RANDOM_VALUE);
+            mathResult = (MathResult) savedInstanceState.get(MATH_RESULT);
+            startTimer = (int) savedInstanceState.getInt(START_TIMER);
+
+            // The dialog of results was shown?
+            if (savedInstanceState.getBoolean("dialogResults")) {
+                showResult();
+            }
+        }
+        list = randomValue.getListAnswer();
+
+        loadPreferences(MathOperation.DIVISION);
+
+        //filling Activity
+        fillingActivity();
+
+    }
+}
